fee7b6a82a868d94e9fee3aea57b0a0b028c4355
sivaramkumar91 <79253987+sivaramkumar91@users.noreply.github.com>
2024-08-20 00:46:50 -0500

    Add files via upload

---
+++ Week1Day2/CheckNumberIsPositive.java
@@ -0,0 +1,15 @@
+package week.day2;
+
+public class CheckNumberIsPositive {
+	public static void main(String[] args) {
+		int Number = 10;
+		if (Number>0) {
+			System.out.println("Number is Positive");		
+		}else if (Number<0) {
+			System.out.println("Number is Negative");
+		}else{
+			System.out.println("Number is Zero");
+		}
+	}
+
+}
